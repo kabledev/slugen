ba3ab14a21b5a285dc7d0e56b2284e6443161a53
kabledev <101965969+kabledev@users.noreply.github.com>
2026-01-04 04:47:10 +0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 export { slugen } from "./slugen";
 export type { SlugenOptions } from "./types";
 export { locales } from "./locales";
-
-// usage:
-// import { slugen } from 'slugen'
-// slugify("İstanbul Boğazı", { locale: 'tr' }) => "istanbul-bogazi"
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
+  target: "es2024",
+  outDir: "dist",
   dts: true,
   sourcemap: true,
   clean: true,
   treeshake: true,
-  target: "es2024",
   external: [],
-  outDir: "dist",
 });
